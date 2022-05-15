3ece372f5501779a87389da65d5431813803b5b9
zsj7012 <zsj7012>
2022-05-15 23:41:56 +0800

    update

--- models/userinfo.go
+++ models/userinfo.go
@@ -6,7 +6,7 @@ type Userinfo struct {
 	Id bson.ObjectId `bson:"_id" json:"id"`
 	User_id string `bson:"user_id" json:"user_id"`
 	Name string `bson:"name" json:"name"`
-	Avatar string	`bson:"avatar" json:"avatar"`
+	Photo_id string	`bson:"photo_id" json:"photo_id"`
 	Birth_at string `bson:"birth_at" json:"birth_at"`
 	Sex string `bson:"sex" json:"sex"`
 	Auth_ids []int `bson:"auth_ids" json:"auth_ids"`
