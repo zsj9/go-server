1f3c6245e91ef826a7c4a23b6c6515d0d3956047
zsj7012 <zsj7012>
2022-06-26 01:28:36 +0800

    tj2

--- app.go
+++ app.go
@@ -3,18 +3,18 @@ package main
 import (
 	"net/http"
 	"restful/routes"
-	"restful/controllers/user"
-	"restful/controllers/userinfo"
+	// "restful/controllers/user"
+	// "restful/controllers/userinfo"
 )
 
 func main() {
 	r := routes.NewRouter()
 
-	r.HandleFunc("/user/register", user.Register).Methods("POST")
-	r.HandleFunc("/user/login", user.Login).Methods("POST")
+	// r.HandleFunc("/user/register", user.Register).Methods("POST")
+	// r.HandleFunc("/user/login", user.Login).Methods("POST")
 
-	r.HandleFunc("/userinfo/add", userinfo.Add_userinfo).Methods("POST")
-	r.HandleFunc("/user/login", userinfo.Find_userinfo).Methods("POST")
+	// r.HandleFunc("/userinfo/add", userinfo.Add_userinfo).Methods("POST")
+	// r.HandleFunc("/user/login", userinfo.Find_userinfo).Methods("POST")
 
 	http.ListenAndServe(":8080", r)
 }
